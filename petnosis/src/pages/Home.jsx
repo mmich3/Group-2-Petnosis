f4321a9d0dd7c7b03fd1eaba6c34360db3e1f3d3
Nikos <nikomvp03@gmail.com>
2025-04-22 13:48:20 -0500

    New home page

--- petnosis/src/pages/Home.jsx
+++ petnosis/src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Home.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 function Home() {
   return (
-    <>
-        <div>Homeeee</div>
-        <Link to="/Blank"><button>Go to Blank</button></Link>
-    </>
-  )
+    <div className='min-h-screen w-75 bg-[#fff6ed]'>
+      <div className='hero min-h-screen'>
+        <div className='hero-content text-neutral-content text-center'>
+          <div className='flex max-w-md flex-col items-center'>
+            <h1 className='mb-5 text-5xl font-bold text-[#ca9973]'>Welcome!</h1>
+            <button className='btn btn-success mb-5'>Pet Diagnosis</button>
+            <button className='btn btn-info mb-5'>Find A Vet</button>
+            <button className='btn btn-error mb-5'>Add Pet</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
