5cd4b2a8eb82d09f32f105f26f89410e16e70702
prativa101 <pkhati2@illinois.edu>
2025-04-23 02:03:43 -0500

    add dog/pet

--- petnosis/src/App.jsx
+++ petnosis/src/App.jsx
@@ -3,6 +3,8 @@ import { useRoutes } from 'react-router-dom';
 import Home from './pages/Home';
 import PetType from './pages/AddPet';
 import AddPet from './pages/AddPet';
+import AddDog from './pages/AddDog';
+import AddCat from './pages/AddCat';
 
 function App() {
   let element = useRoutes([
@@ -13,6 +15,14 @@ function App() {
     {
       path: '/add-pet',
       element: <AddPet />,
+    },
+    {
+      path: '/add-dog',
+      element: <AddDog />,
+    },
+    {
+      path: '/add-cat',
+      element: <AddCat />,
     },
   ]);
 
--- petnosis/src/pages/AddPet.jsx
+++ petnosis/src/pages/AddPet.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AddPet = () => {
   return (
     <div className='min-h-screen'>
-      <div className='flex min-h-screen flex-col justify-around'>
+      <div className='flex min-h-screen flex-col items-center justify-around'>
         <div className='hero-content'>
-          <p className='mb-5 text-5xl font-bold text-[#ca9973]'>What Type of Pet?</p>
+          <p className='mt-35 text-5xl font-bold text-[#ca9973]'>What Type of Pet?</p>
         </div>
-        <div className='hero'>
-          <div className='hero-content flex'>
-            <button className='btn btn-accent'>Cat</button>
-            <button className='btn btn-secondary'>Dog</button>
+
+        <div className ='mt-20 flex flex-col items-center space-y-4'>
+          <div>
+            <Link className='mb-10' to='/add-cat'>
+              <button className='btn btn-accent h-15 w-25 text-xl font-bold'>Cat</button>
+            </Link>
+            
+          
           </div>
+          <div className='mt-10 mb-100 '>
+            
+            <Link className='mb-15' to='/add-dog'>
+              <button className='btn btn-secondary h-15 w-25 text-xl font-bold'>Dog</button>
+            </Link>
+            
+            
+          </div>
+
         </div>
+        
       </div>
     </div>
   );
