fed2622b4df89f0782253f9e30fa599cef9baafc
Rogelio Lozano <rjlozano58@gmail.com>
2025-04-24 13:22:52 -0500

    changed Location to LocationInfo.

---
+++ petnosis/src/pages/LocationInfo.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Select from 'react-select';
+
+const LocationInfo = () => {
+  return (
+    <div className='min-h-screen'>
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <div className='hero-content'>
+        <div className='flex items-center justify-center gap-8'>
+          <div>
+            <label className='mb-2 block w-[200px] text-lg font-semibold text-gray-700'>
+              Street Address:
+            </label>
+            <input
+              type='text'
+              className='input input-bordered w-full max-w-xs'
+              value={name}
+              placeholder='Ex: John'
+
+            />
+            <label className='mb-2 block w-[200px] text-lg font-semibold text-gray-700'>
+              City:
+            </label>
+            <input
+              type='text'
+              className='input input-bordered w-full max-w-xs'
+              value={name}
+              placeholder='Ex: John'
+
+            />
+            <label className='mb-2 block w-[200px] text-lg font-semibold text-gray-700'>
+              State:
+            </label>
+            <input
+              type='text'
+              className='input input-bordered w-full max-w-xs'
+              value={name}
+              placeholder='Ex: John'
+
+            />
+
+<label className='mb-2 block w-[200px] text-lg font-semibold text-gray-700'>
+              ZipCode:
+            </label>
+            <input
+              type='text'
+              className='input input-bordered w-full max-w-xs'
+              value={name}
+              placeholder='Ex: John'
+
+            />
+          </div>
+
+          
+        </div>
+        </div>
+        <div>
+          <div className='hero-content mt-10'>
+            <Link to='/PickAVet'>
+              <button className='btn btn-secondary'>Next</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default LocationInfo;
