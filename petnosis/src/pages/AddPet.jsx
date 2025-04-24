f95f55f61d3e54f3cc100abd34a630119cf36550
Nikos <nikomvp03@gmail.com>
2025-04-24 13:20:02 -0500

    Add back button component

--- petnosis/src/pages/AddPet.jsx
+++ petnosis/src/pages/AddPet.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { FaChevronLeft } from 'react-icons/fa';
 
 const AddPet = () => {
   return (
     <div className='min-h-screen'>
+      <button className='btn btn-outline btn-circle border-black text-black hover:text-white m-2 fixed'>
+        <FaChevronLeft />
+      </button>
       <div className='flex min-h-screen flex-col items-center justify-center'>
         <div className='hero-content mb-5'>
           <p className='text-5xl font-bold text-[#ca9973]'>What Type of Pet?</p>
@@ -14,8 +18,8 @@ const AddPet = () => {
             <button className='btn btn-xl btn-secondary'>Dog</button>
           </div>
         </div>
+        <Navbar />
       </div>
-      <Navbar />
     </div>
   );
 };
--- petnosis/src/pages/AdditionalSymptoms.jsx
+++ petnosis/src/pages/AdditionalSymptoms.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { FaChevronLeft } from 'react-icons/fa';
 
 const AdditionalSymptoms = () => {
   return (
