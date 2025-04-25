f5f172dc742db627c8c5f0c585c7ee58a40bc99f
Nikos <nikomvp03@gmail.com>
2025-04-25 18:53:56 -0500

    Fix layout of dog and cat pages

--- petnosis/src/components/Navbar.jsx
+++ petnosis/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='absolute bottom-0 z-10 flex w-full justify-between bg-[#8fe3ff] shadow-md'>
+    <nav className='bottom-0 z-10 mt-2 flex w-full justify-between bg-[#8fe3ff] shadow-md'>
       <Link className='btn btn-lg btn-success m-0.5 flex-1 text-white' to='/PetSymptoms'>
         <FaPlus />
         Diagnosis
