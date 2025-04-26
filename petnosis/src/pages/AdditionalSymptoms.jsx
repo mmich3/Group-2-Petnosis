df64d20225f3723f508fc9c4eed0aac391f004b1
mmich3 <mmich3@uic.edu>
2025-04-25 21:42:23 -0500

    Updated colors for additional symptoms

--- petnosis/src/pages/AdditionalSymptoms.jsx
+++ petnosis/src/pages/AdditionalSymptoms.jsx
@@ -13,23 +13,23 @@ const AdditionalSymptoms = () => {
         </div>
 
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-            <b style={{marginBottom: 10, fontSize:40}}>Log Duration</b>
+            <b style={{marginBottom: 10, fontSize:40, color: 'green'}}>Log Duration</b>
             <div>
-                <p style={{textAlign: 'center', fontSize: 35}}>1. Limping</p>
+                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>1. Limping</p>
                 <div style={{display: 'flex', marginBottom: 10}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
                     <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
                     <p style = {{paddingLeft: 10, padding: 3, fontSize: 25}}>To:</p>
                     <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
                 </div>
-                <p style={{textAlign: 'center', fontSize: 35}}>2. Coughing</p>
+                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>2. Coughing</p>
                 <div style={{display: 'flex', marginBottom: 10}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
                     <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
                     <p style = {{paddingLeft: 10, padding: 3, fontSize: 25}}>To:</p>
                     <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
                 </div>
-                <p style={{textAlign: 'center', fontSize: 35}}>3. Lack of appetite</p>
+                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>3. Lack of appetite</p>
                 <div style={{display: 'flex', marginBottom: 10}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
                     <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
@@ -41,7 +41,7 @@ const AdditionalSymptoms = () => {
 
         <div style={{marginTop: 100, fontSize: 23}}>
             <p>-Optional-</p>
-            <p>DESCRIPTION:</p>
+            <p style={{color: '#46A3FF'}}>DESCRIPTION:</p>
             <input type='text' style={{border: '1px solid black', borderRadius: '9px' ,width: 330, height: 40, paddingLeft: 20}} onFocus={(e) => e.target.value = ''} defaultValue="Start typing..."></input>
         </div>
 
