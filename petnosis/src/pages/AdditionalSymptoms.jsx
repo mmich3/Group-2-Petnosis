64d8b5c4dc8cd438799120a4551745489522399e
mmich3 <mmich3@uic.edu>
2025-04-25 21:58:49 -0500

    Updated better styling for additional symptoms

--- petnosis/src/pages/AdditionalSymptoms.jsx
+++ petnosis/src/pages/AdditionalSymptoms.jsx
@@ -12,35 +12,35 @@ const AdditionalSymptoms = () => {
           <p className='text-5xl font-bold text-[#ca9973]'>Additional Info</p>
         </div>
 
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-            <b style={{marginBottom: 10, fontSize:40, color: 'green'}}>Log Duration</b>
-            <div>
-                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>1. Limping</p>
-                <div style={{display: 'flex', marginBottom: 10}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: '#dcdcef', borderRadius: '8px'}}>
+            <b style={{marginBottom: 10, fontSize:40, color: '#46A3FF'}}>Log Duration</b>
+            <div style={{textAlign: 'center'}}>
+                <b style={{textAlign: 'center', fontSize: 35, color:'#4B7A82'}}>1. Limping</b>
+                <div style={{display: 'flex', marginBottom: 10, padding: 15}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                     <p style = {{paddingLeft: 10, padding: 3, fontSize: 25}}>To:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                 </div>
-                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>2. Coughing</p>
-                <div style={{display: 'flex', marginBottom: 10}}>
+                <b style={{textAlign: 'center', fontSize: 35, color:'#4B7A82'}}>2. Coughing</b>
+                <div style={{display: 'flex', marginBottom: 10, padding: 15}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                     <p style = {{paddingLeft: 10, padding: 3, fontSize: 25}}>To:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                 </div>
-                <p style={{textAlign: 'center', fontSize: 35, color:'#46A3FF'}}>3. Lack of appetite</p>
-                <div style={{display: 'flex', marginBottom: 10}}>
+                <b style={{textAlign: 'center', fontSize: 35, color:'#4B7A82'}}>3. Lack of appetite</b>
+                <div style={{display: 'flex', marginBottom: 10, padding: 15}}>
                     <p style = {{padding: 3, fontSize: 25}}>From:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                     <p style = {{paddingLeft: 10, padding: 3, fontSize: 25}}>To:</p>
-                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15}}></input>
+                    <input type='date' style={{border: '1px solid black', borderRadius: '4px' ,width: 110, fontSize:15, color: 'grey', backgroundColor: 'white'}}></input>
                 </div>
             </div>
         </div>
 
-        <div style={{marginTop: 100, fontSize: 23}}>
-            <p>-Optional-</p>
+        <div style={{marginTop: 50, fontSize: 23}}>
+            <p style={{color: 'grey'}}>-Optional-</p>
             <p style={{color: '#46A3FF'}}>DESCRIPTION:</p>
             <input type='text' style={{border: '1px solid black', borderRadius: '9px' ,width: 330, height: 40, paddingLeft: 20}} onFocus={(e) => e.target.value = ''} defaultValue="Start typing..."></input>
         </div>
